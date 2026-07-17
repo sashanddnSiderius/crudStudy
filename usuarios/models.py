12b9bffd8a79829ddbce682766ae7da47f9e0cca
sashanddnSiderius <therealjonnye145@gmail.com>
2026-07-17 15:25:45 -0300

    feat: adiciona campo telefone e gera migrations

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,7 +5,8 @@ class Usuario(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=50, unique=True, null=False, blank=False)
     cpf = models.CharField(max_length=11,unique=True, null=False, blank=False)
-    endereco= models.CharField(max_length=100, null=False, blank=False)
+    telefone = models.CharField(max_length=15, default='Sem telefone')
+    endereco = models.CharField(max_length=100, null=False, blank=False)
     data_nascimento = models.DateField(null=False, blank=False)
 
 def __str__(self):
