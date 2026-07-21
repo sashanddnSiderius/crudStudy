8c59deeee24cbda5efb096bdf3cf5e9a16cd78ec
sashanddnSiderius <therealjonnye145@gmail.com>
2026-07-21 10:12:29 -0300

    Cria forms.py para formulário de usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from .models import Usuario
+from .forms import UsuarioForm
 
 # Create your views here.
 def criar_usuario(request):
-    return render(request, 'usuarios/criar_usuario.html')
+    
+    if request.method == "POST":
+        
+        form = UsuarioForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+    
+    else:
+        
+        form = UsuarioForm()
+    
+    return render(request, 'usuarios/criar_usuario.html', {"form": form})       
 
 def listar_usuarios(request):
 
     usuarios = Usuario.objects.all()
     
     return render(request, 'usuarios/listar_usuarios.html', {"usuarios": usuarios})
+
